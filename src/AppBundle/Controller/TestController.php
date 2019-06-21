5db10464cf6931320cbfbf5ad061f406d51cff09
Georg Preissl <fampreissl@gmail.com>
2019-06-21 15:06:28 +0200

    fix

--- src/AppBundle/Controller/TestController.php
+++ src/AppBundle/Controller/TestController.php
@@ -13,14 +13,31 @@ use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 
+
+
+
 class TestController extends Controller
 {
 
-
-    public function loadAction()
+    /**
+     * @Route("/test/")
+     */
+    public function loadAction($height, $width)
     {
-
-    	return "test";
+        $strBuffer = '<!DOCTYPE html>
+<html lang="de">
+<head>
+  <meta charset="utf-8">
+  <title>Frontend Route - Contao Open Source CMS</title>
+</head>
+<body>
+       <h1>Frontend Route</h2>
+	        <p>Parameter: '.$height.' '.$width.'</p>
+</body>
+</html>';
+        $objResponse = new Response($strBuffer);
+        $objResponse->headers->set('Content-Type', 'text/html; charset=UTF-8');
+        return $objResponse;
 
     }
 }
